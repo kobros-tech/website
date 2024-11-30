99864bf4eabedb4092a2220e4ecd3a1375926355
kobros-tech <alkobroslymohamed@gmail.com>
2024-11-30 03:02:47 +0300

    [17.0][MIG] website_search_header: Migration to 17.0

--- website_search_header/static/src/js/website.js
+++ website_search_header/static/src/js/website.js
@@ -1,62 +1,43 @@
-odoo.define("website_search_header.search_header", function (require) {
+(function () {
     "use strict";
+    function mobileSearch() {
+        if (!this.$boxInput.hasClass("d-none")) {
+            this.$boxInput.addClass("d-none");
+        }
+        var searchQuery = document.querySelector("input[name='search']").value;
+        var encodedQuery = encodeURIComponent(searchQuery);
+        var searchURL = this._getSearchUrl(encodedQuery);
+        window.location.href = searchURL;
+    }
 
-    const publicWidget = require("web.public.widget");
+    function desktopSearch() {
+        if ($(".o_search_header").hasClass("d-lg-none")) {
+            $(".o_search_header").removeClass("d-lg-none");
+            $("#search_btn_toggle_search i").removeClass("oi-search");
+            $("#search_btn_toggle_search i").addClass("oi-close");
+            $("#div_search_header").removeClass("ms-lg-0");
+            $("#div_search_button").removeClass("ms-lg-2");
+            $("input.oe_search_box").focus();
+        } else {
+            $(".o_search_header").addClass("d-lg-none");
+            $("#search_btn_toggle_search i").removeClass("oi-close");
+            $("#search_btn_toggle_search i").addClass("oi-search");
+            $("#div_search_header").addClass("ms-lg-0");
+            $("#div_search_button").addClass("ms-lg-2");
+        }
+    }
 
-    publicWidget.registry.SearchHeader = publicWidget.Widget.extend({
-        selector: "#div_search_button",
+    function searchHeader() {
+        var screenWidth =
+            window.innerWidth ||
+            document.documentElement.clientWidth ||
+            document.body.clientWidth;
+        if (screenWidth < 992) {
+            mobileSearch();
+        } else {
+            desktopSearch();
+        }
+    }
 
-        events: {
-            "click  #search_btn_toggle_search": "_onClickSearchBtn",
-        },
-
-        start: function () {
-            const def = this._super.apply(this, arguments);
-            this.$boxInput = this.$el.find(".o_search_header");
-            return def;
-        },
-
-        mobileSearch: function () {
-            if (!this.$boxInput.hasClass("d-none")) {
-                this.$boxInput.addClass("d-none");
-            }
-            var searchQuery = document.querySelector("input[name='search']").value;
-            var encodedQuery = encodeURIComponent(searchQuery);
-            var searchURL = this._getSearchUrl(encodedQuery);
-            window.location.href = searchURL;
-        },
-
-        desktopSearch: function () {
-            if (!$(".o_search_header").hasClass("d-lg-none")) {
-                $(".o_search_header").addClass("d-lg-none");
-                $("#search_btn_toggle_search i").removeClass("oi-close");
-                $("#search_btn_toggle_search i").addClass("oi-search");
-                $("#div_search_header").addClass("ms-lg-0");
-                $("#div_search_button").addClass("ms-lg-2");
-            } else {
-                $(".o_search_header").removeClass("d-lg-none");
-                $("#search_btn_toggle_search i").removeClass("oi-search");
-                $("#search_btn_toggle_search i").addClass("oi-close");
-                $("#div_search_header").removeClass("ms-lg-0");
-                $("#div_search_button").removeClass("ms-lg-2");
-                $("input.oe_search_box").focus();
-            }
-        },
-
-        _onClickSearchBtn: function () {
-            var screenWidth =
-                window.innerWidth ||
-                document.documentElement.clientWidth ||
-                document.body.clientWidth;
-            if (screenWidth < 992) {
-                this.mobileSearch();
-            } else {
-                this.desktopSearch();
-            }
-        },
-
-        _getSearchUrl: function (encodedQuery) {
-            return "/website/search?search=" + encodedQuery + "&amp;order=name+asc";
-        },
-    });
-});
+    window.searchHeader = searchHeader;
+})();
